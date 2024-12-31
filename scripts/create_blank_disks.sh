f6558ca34bce21fc08ff5e0d48813aa7bdf2e3de
Brad Searle <bradleysearle@gmail.com>
2024-12-31 13:13:22 +0000

    create disk script

--- scripts/create_blank_disks.sh
+++ scripts/create_blank_disks.sh
@@ -16,5 +16,17 @@ cp base.img junos.img
 mkfs.vfat  -v -n "vmm-data"
 
 # EXT4 3GB
+qemu-img create -f raw ext4-1g.img 1G
+mkfs.ext4 -L "data-disk" ext4-1g.img
+
+qemu-img create -f raw ext4-2g.img 2G
+mkfs.ext4 -L "data-disk" ext4-2g.img
+
 qemu-img create -f raw ext4-3g.img 3G
 mkfs.ext4 -L "data-disk" ext4-3g.img
+
+qemu-img create -f raw ext4-4g.img 4G
+mkfs.ext4 -L "data-disk" ext4-4g.img
+
+qemu-img create -f raw ext4-5g.img 5G
+mkfs.ext4 -L "data-disk" ext4-5g.img
